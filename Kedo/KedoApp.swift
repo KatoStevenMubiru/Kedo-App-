e74fe8e9cdd4e6c8b7c2d7e5ce1adbbb13d993bb
Kato Steven Mubiru <kato.steven60@gmail.com>
2022-11-29 07:08:40 +0300

    Initial Commit

---
+++ Kedo/KedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KedoApp.swift
+//  Kedo
+//
+//  Created by Kato Steven Mubiru on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct KedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
